d2d3a5a02f81c5b895aed2a1e1d6035fab52eb3d
16ozcoldbru <osborna2@oregonstate.edu>
2022-11-09 21:13:53 -0800

    added a bunch of stuff

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 import os
+import database.db_connector as db
 
 # Configuration
 
 app = Flask(__name__)
+db_connection = db.connect_to_database()
 
 # Routes 
 
 @app.route('/')
 def root():
     return render_template("main.j2")
+
+@app.route('/dinosaurs')
+def dinosaurs():
+    # Write the query and save it to a variable
+    query = "SELECT * FROM Dinosaurs;"
+
+    # The way the interface between MySQL and Flask works is by using an
+    # object called a cursor. Think of it as the object that acts as the
+    # person typing commands directly into the MySQL command line and
+    # reading them back to you when it gets results
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    # The cursor.fetchall() function tells the cursor object to return all
+    # the results from the previously executed
+    results = cursor.fetchall()
+
+    # Sends the results back to the web browser.
+    # return results
+    return render_template("dinosaurs.j2", Dinosaurs=results)
 
 # Listener
 
